c2549637b7c9a1ae516e5a61d259bb5b07f0160d
iyappank <krishnan.iyappan@gmail.com>
2026-01-07 18:18:26 +0400

    testing frontend

---
+++ Frontend/vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
+  ],
+  server: {
+    host: true, // or '0.0.0.0' or a specific IP address
+    port: 3000
+  }
+})
