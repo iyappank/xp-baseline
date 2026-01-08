81108ea6714bed943872d6d4f44b8459e3a8fd4a
iyappank <krishnan.iyappan@gmail.com>
2026-01-08 22:29:03 +0400

    frontend env changes

--- Frontend/api/cmsApi.js
+++ Frontend/api/cmsApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { toast } from "react-toastify";
 
-const CMS_API_URL = process.env.REACT_APP_CMS_URL ? process.env.REACT_APP_CMS_URL : "http://localhost:1337";
+const CMS_API_URL = import.meta.env.REACT_APP_CMS_URL ? import.meta.env.REACT_APP_CMS_URL : "http://localhost:1337";
 
 export const fetchCMSContent = async () => {
   try {
--- Frontend/api/productApi.js
+++ Frontend/api/productApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { toast } from "react-toastify";
 
-const BACKEND_API_URL = process.env.REACT_APP_API_URL ? process.env.REACT_APP_API_URL :  "http://localhost:8080";
+const BACKEND_API_URL = import.meta.env.REACT_APP_API_URL ? import.meta.env.REACT_APP_API_URL :  "http://localhost:8080";
 
 export const fetchProducts = async () => {
   try {
